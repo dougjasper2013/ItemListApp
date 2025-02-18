93b7ed2c9447379a74e284091b427eecdbdb73e5
Douglas Jasper <doug.jasper@trios.com>
2025-02-18 08:13:12 -0500

    Initial Commit

---
+++ ItemListApp/ItemListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItemListAppApp.swift
+//  ItemListApp
+//
+//  Created by Douglas Jasper on 2025-02-18.
+//
+
+import SwiftUI
+
+@main
+struct ItemListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
